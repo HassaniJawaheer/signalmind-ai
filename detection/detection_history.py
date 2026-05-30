285c5f9d4b694dd111f8fb7bef38e4d4b1a5da09
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2026-05-30 19:24:24 +0200

    [feat] finalize detection module refactor (rules, detector, and models)

---
+++ detection/detection_history.py
@@ -0,0 +1,46 @@
+import json
+from dataclasses import asdict
+
+from config.settings import (
+    SAVE_ALL_DETECTIONS,
+    DETECTION_HISTORY_PATH
+)
+
+
+class DetectionHistory:
+
+
+    def __init__(self):
+
+        self.path = DETECTION_HISTORY_PATH
+
+
+    def save(self, signal):
+
+
+        if not SAVE_ALL_DETECTIONS:
+
+            if not signal.trigger:
+
+                return
+
+
+        with open(
+
+            self.path,
+
+            "a",
+
+            encoding="utf-8"
+
+        ) as f:
+
+
+            json.dump(
+
+                asdict(signal),
+
+                f
+            )
+
+            f.write("\n")
